c62c7312f240646c107f902f8b759fcceb50deb9
Александр Литвинов <sbawalexlit2@gmail.com>
2022-04-05 14:37:34 +0300

    dz

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры или задает самостоятельно M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Console.WriteLine("сколько чисел хотите ввести?");
+// int len = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("вводите");
+// int a = 0;
+// void metod(int arr)
+// {
+//     for (int i = 0; i < arr; i++)
+//     {
+//         int numbers = Convert.ToInt32(Console.ReadLine());
+//         if (numbers > 0)
+//             a += 1;
+//     }
+// }
+// metod(len);
+// Console.WriteLine($"из них положительных  {a}");
+
+
+
+
+
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем или задаются вручную.
+
+
+// Console.Write("Введите b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = (b2 - b1) / (k1 - k2);
+// double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+
+// Console.WriteLine($"точка пересечения двух прямых x= {x}, y= {y}");
